b3b45b1d61b8864a8a362b645f66d91f7ad05c46
Hunter Charvat <charvat@umich.edu>
2016-11-11 01:09:45 -0500

    Challenge B almost complete

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -21,9 +21,12 @@ Deliverables:
 
 CITATIONS:
 ---------------------------------------------------------------------
-(1):
-        -
-        -
+(1): Stackoverflow users Mark Ransom
+        - How to create a file from Python
+        - http://stackoverflow.com/questions/16346914/python-3-2-unicodeencodeerror-charmap-codec-cant-encode-character-u2013-i
+(2): Stackoverflow user Martin v. Lowis
+        - How to make a raw string
+        - http://stackoverflow.com/questions/1347791/unicode-error-unicodeescape-codec-cant-decode-bytes-cannot-open-text-file
 ---------------------------------------------------------------------
 
 """
@@ -31,12 +34,23 @@ CITATIONS:
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
-import re
+import os
 
-base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
+fol = os.path.dirname(__file__)
+img_file = filename = os.path.join(fol, '/media/logo.png')
+
+print(img_file)
+
+base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "lxml")
+for x in soup.find_all('img'):
+    if 'testbed' not in x['src']:
+        x['src'] = r'C:\Users\charv_000\proj3\HW3-StudentCopy\media\logo.png'
+
 clean = soup.prettify()
 rep1 = clean.replace('student', 'AMAZING student')
+rep2 = rep1.replace("https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg", "hunter.jpg")
 
-
+html_file = open('new.html', 'w', encoding='utf-8')
+html_file.write(rep2)
