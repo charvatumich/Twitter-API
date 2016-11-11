e6eb47ced3fa7df923fa92bc2de2538efd4dbfa8
Hunter Charvat <charvat@umich.edu>
2016-11-11 16:55:50 -0500

    Fixed os bug in challenge A, moved other challenges to proper file.

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -1,52 +1,45 @@
-"""
-PROJECT 3; CHALLENGE A:
-BY: Hunter Charvat
-
-CHALLENGE SPECIFICATIONS:
----------------------------------------------------------------------
-Use https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions
-    as a template.
-
-STEPS
-Create a similar HTML file but
-1) Replace every occurrence of the word “student” with “AMAZING
-student.”
-2) Replace the main picture with a picture of yourself.
-3) Replace any local images with the image I provided in media.  (You
-must keep the image in a separate folder than your html code.
-
-Deliverables:
-1) Make sure the new page is uploaded to your GitHub account.
----------------------------------------------------------------------
-
-CITATIONS:
----------------------------------------------------------------------
-(1): Stackoverflow users Mark Ransom
-        - How to create a file from Python
-        - http://stackoverflow.com/questions/16346914/python-3-2-unicodeencodeerror-charmap-codec-cant-encode-character-u2013-i
-(2): Stackoverflow user Martin v. Lowis
-        - How to make a raw string
-        - http://stackoverflow.com/questions/1347791/unicode-error-unicodeescape-codec-cant-decode-bytes-cannot-open-text-file
----------------------------------------------------------------------
-
-"""
+# PROJECT 3; CHALLENGE A:
+# BY: Hunter Charvat
+#
+# CHALLENGE SPECIFICATIONS:
+# ---------------------------------------------------------------------
+# Use https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions
+#     as a template.
+#
+# STEPS
+# Create a similar HTML file but
+# 1) Replace every occurrence of the word “student” with “AMAZING
+# student.”
+# 2) Replace the main picture with a picture of yourself.
+# 3) Replace any local images with the image I provided in media.  (You
+# must keep the image in a separate folder than your html code.
+#
+# Deliverables:
+# 1) Make sure the new page is uploaded to your GitHub account.
+# ---------------------------------------------------------------------
+#
+# CITATIONS:
+# ---------------------------------------------------------------------
+# (1): Stackoverflow users Mark Ransom
+#         - How to create a file from Python
+#         - http://stackoverflow.com/questions/16346914/python-3-2-unicodeencodeerror-charmap-codec-cant-encode-character-u2013-i
+# (2): Stackoverflow user Martin v. Lowis
+#         - How to make a raw string
+#         - http://stackoverflow.com/questions/1347791/unicode-error-unicodeescape-codec-cant-decode-bytes-cannot-open-text-file
+# ---------------------------------------------------------------------
+#
 
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
 import os
 
-fol = os.path.dirname(__file__)
-img_file = filename = os.path.join(fol, '/media/logo.png')
-
-print(img_file)
-
 base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "lxml")
 for x in soup.find_all('img'):
     if 'testbed' not in x['src']:
-        x['src'] = r'C:\Users\charv_000\proj3\HW3-StudentCopy\media\logo.png'
+        x['src'] = os.path.abspath(r'media\logo.png')
 
 clean = soup.prettify()
 rep1 = clean.replace('student', 'AMAZING student')
--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -1,12 +1,54 @@
-# In this assignment you must do a Twitter search on any term
-# of your choice.
+# """
+# PROJECT 3; CHALLENGE D:
+# BY: Hunter Charvat
+#
+# CHALLENGE SPECIFICATIONS:
+# ---------------------------------------------------------------------
+# In this assignment you must do a Twitter search on any term of your choice.
+# Be prepared to change the search term during demo.
+#
 # Deliverables:
 # 1) Print each tweet
 # 2) Print the average subjectivity of the results
 # 3) Print the average polarity of the results
+# ---------------------------------------------------------------------
+#
+# CITATIONS:
+# ---------------------------------------------------------------------
+# (1): Stackoverflow user Martijn Pieters
+#         - How to print without spaces between values
+#         - http://stackoverflow.com/questions/28669459/how-to-print-variables-without-spaces-between-values
+# ---------------------------------------------------------------------
+# """
 
-# Be prepared to change the search term during demo.
+import configparser
+import tweepy
+import nltk
+from textblob import TextBlob
 
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Store personal info in untracked file and get it
+config = configparser.ConfigParser()
+config.read('config.ini')
+inf = config['twitter_info']
+AT = inf['at']
+ATS = inf['ats']
+CK = inf['ck']
+CS = inf['cs']
+
+SEARCH = "Love"
+
+# Create an instance of tweepy twitter API
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, ATS)
+api = tweepy.API(auth)
+
+results = api.search(q=SEARCH)
+c = 1
+for tweet in results:
+    print(str(c) + ')')
+    print(tweet.text)
+    print('*****')
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment, '\n')
+    c += 1
